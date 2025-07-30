561b56951c162f81afdb4b61816c4949a743ab0c
minjeong44 <23010102@pusangangseo.hs.kr>
2025-07-30 11:34:36 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -16,7 +16,7 @@ st.write('값:', values)
 
 st.subheader('시간 범위 슬라이더')
 
-appoimtment = st.slider(
+appointment = st.slider(
     "약속을 예약하세요:",
     value=(time(11,30), time(12,45)))
 st.write("예약된 시간:", appointment)
